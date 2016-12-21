f81274aef81339ffa77e382af54c90c799accad3
vkarppinen <karppinenvaltteri@gmail.com>
2016-12-21 17:07:49 +0200

    minor changes

--- module-1/app.js
+++ module-1/app.js
@@ -9,24 +9,21 @@
   function LunchController($scope) {
     $scope.lunch = "";
     $scope.message = "";
-    $scope.custom = {}
+    $scope.fontColor = "";
 
     $scope.checkLunch = function () {
       var lunchItems = $scope.lunch.split(",");
       if ($scope.lunch == "") {
         $scope.message = "Please enter data first.";
-        $scope.custom.color = "color: red;";
-        $scope.custom.borderColor = "border-color: red";
+        $scope.fontColor = "text-danger";
       }
       else if (lunchItems.length <= 3) {
         $scope.message = "Enjoy!";
-        $scope.custom.color = "color: green;";
-        $scope.custom.borderColor = "border-color: green";
+        $scope.fontColor = "text-success";
       }
       else if (lunchItems.length >= 4) {
         $scope.message = "Too much!";
-        $scope.custom.color = "color: green;";
-        $scope.custom.borderColor = "border-color: green";
+        $scope.fontColor = "text-success";
       }
 
 
