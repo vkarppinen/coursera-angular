6f69c0f86b066ddbd3b96cfe49bd3728dcb51444
vkarppinen <karppinenvaltteri@gmail.com>
2017-01-05 17:38:07 +0200

    created a loader that shows up on async request

--- module-4/src/categories/categories.controller.js
+++ module-4/src/categories/categories.controller.js
@@ -5,24 +5,33 @@ angular.module('MenuApp')
 .controller('MenuController', MenuController);
 
 
-MenuController.$inject = ['MenuDataService'];
-function MenuController(MenuDataService) {
+MenuController.$inject = ['$rootScope', 'MenuDataService'];
+function MenuController($rootScope, MenuDataService) {
   var menu = this;
   menu.categories = [];
 
+
   menu.$onInit = function () {
-    var response = MenuDataService.getAllCategories();
 
-    response.then( function () {
-      // Catch the menu categories
-      var categories = response.$$state.value.data;
-      menu.categories = categories
-    })
-    .catch(function (error) {
-      console.log(error);
-    })
+    $rootScope.$broadcast('menu:processing', {loaderOn: true});
+
+    // 1500 millisec timeout to demonstrate the async call.
+    setTimeout(function(){
+      var response = MenuDataService.getAllCategories();
+      response.then( function () {
+        // Catch the menu categories
+        var categories = response.$$state.value.data;
+        menu.categories = categories;
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+      .finally(function () {
+        $rootScope.$broadcast('menu:processing', {loaderOn: false});
+      });
+    }, 1500);
+
   };
-
 }
 
 })();
